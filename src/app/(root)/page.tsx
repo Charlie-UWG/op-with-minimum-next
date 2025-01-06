a675d2f7de051224db16ccc845b2a7c91fbbeac3
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2025-01-06 11:53:11 +0900

    🎨 hero画像の表示

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import hero from '~/assets/hero-sample-01.jpg'
 import Header from '~/components/Header'
 
 export default function Home() {
@@ -6,13 +8,23 @@ export default function Home() {
       {/* <img id="background" src={background.src} alt="" fetchpriority="high" /> */}
       <main>
         <Header />
-        <h2 className="mt-8 flex justify-center text-xl font-bold lg:text-5xl">
+        <h2 className="mt-8 flex justify-center text-xl font-bold sm:text-5xl">
           未来へつながるデジタル化
         </h2>
         <h3 className="mt-4 flex justify-center text-lg lg:text-2xl">
           2025年4月 高機能デジタルスキャニングセンターが誕生
         </h3>
-        {/* <img id="hero" src={hero} alt="Hero Sample" className="mt-4 mx-auto sm:max-h-[40vh]"> */}
+        <div className="relative mx-auto mt-4 aspect-[16/9] w-full max-w-4xl">
+          <Image
+            id="hero"
+            src={hero}
+            alt="Hero Sample"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 1200px"
+            className="object-cover"
+            priority
+          />
+        </div>
         <p className="mx-4 mt-6 font-medium lg:mx-24">
           最新鋭のデジタルスキャニング技術を駆使し、紙媒体を効率的にデジタル化。過去の資料を未来へと引き継ぎ、アクセスしやすいデジタル資産に変えます。障害者就労支援を通じて社会貢献も実現しています。
         </p>
